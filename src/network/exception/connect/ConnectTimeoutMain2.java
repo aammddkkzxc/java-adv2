a9e3169cf8a55de28e9f008574260d2896632932
aammddkkzxc <ideal0427@naver.com>
2024-12-05 21:44:26 +0900

    feat: 네트워크 예외2 - 타임아웃 - TCP 연결과 관련된 타임아웃 - 요청에 대해서 응답이 느려질 때 타임아웃 - 외부 서버와 통신을 하는 경우 반드시 연결 타임아웃과 소켓 타임아웃을 지정해야 한다. - 외부 서버에 문제가 있을 경우 응답을 기다리는 서버 스레드가 계속 늘어나 서버 장애까지 이어질 수 있음

---
+++ src/network/exception/connect/ConnectTimeoutMain2.java
@@ -0,0 +1,28 @@
+package network.exception.connect;
+
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
+public class ConnectTimeoutMain2 {
+
+    public static void main(String[] args) throws IOException {
+
+        long startTime = System.currentTimeMillis();
+
+        try {
+            Socket socket = new Socket();
+            socket.connect(new InetSocketAddress("192.168.1.250", 45678), 2000);
+
+        } catch (SocketTimeoutException e) {
+            // ConnectException: Operation timed out
+            e.printStackTrace();
+        }
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("end : " + (endTime - startTime));
+    }
+
+}
